b4d9be4032b4d9bccb830cfbb900f3617dcab467
Heitor <heitorbc3@gmail.com>
2019-08-22 16:30:26 -0300

    criar objetos

---
+++ oop.py
@@ -0,0 +1,33 @@
+# nome = input('Digite seu nome')
+# idade = input('Digite sua idade')
+# rg = input('Digite seu rg')
+
+
+
+class pessoa:
+    def __init__(self,nome,idade,rg):
+        self.nome = nome
+        self.idade = idade
+        self.rg =rg
+
+
+
+
+
+
+class paciente:
+    def __init__(self,enfermidade,nd_risco,sintomas):
+        self.enferidade = enfermidade
+        self.nd_risco = nd_risco
+        self.sintomas = sintomas
+
+
+
+
+
+
+
+
+class medico:
+    def __init__(self,crm):
+        self.crm = crm
